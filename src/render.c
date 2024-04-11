d123d29ecc12d45b12a3afd63fe7fe27a9fb96a4
Albert <albertprpu97@gmail.com>
2024-04-11 11:44:59 +0200

    removed deltatime calculation in render.c, it was in the wrong place

--- src/render.c
+++ src/render.c
@@ -6,28 +6,17 @@
 /*   By: apresas- <apresas-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 12:43:23 by jenavarr          #+#    #+#             */
-/*   Updated: 2024/04/10 13:47:10 by apresas-         ###   ########.fr       */
+/*   Updated: 2024/04/11 11:43:59 by apresas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-// debug
-#include <time.h>
 
 void	render(t_data *data)
 {
 	set_floor_ceiling(data);
 	init_raycasting(data);
 	mlx_put_image_to_window(data->mlx.mlx, data->mlx.window, data->mlx.win_img->img, 0, 0);
-
-// to measure time between frames
-	// static clock_t start;
-	// static clock_t end;
-	// end = clock();
-	// printf("Time since last frame: %f\n", ((double) (end - start)) / CLOCKS_PER_SEC);
-	// start = clock();
-//
 }
 
 void	set_floor_ceiling(t_data *data)

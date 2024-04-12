aea6c3bab8244f51eddb9b714764cceea857c451
Albert <albertprpu97@gmail.com>
2024-04-12 19:36:23 +0200

    doors and sprites bonus start

---
+++ src/sprites.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sprites.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apresas- <apresas-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 19:20:23 by apresas-          #+#    #+#             */
+/*   Updated: 2024/04/12 19:32:30 by apresas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+
+Sprites will be stationary (OBVIOUSLY)
+
+They will be indicated in the map with 'S'.
+
+They will behave like a '0' tile, except there will be a sprite there.
+
+Sprites will always have the same image, nothing special on the map.cub file.
+
+They will always face the player.
+
+They will be given a certain hardcoded height. Depending on the sprite.
+
+*/
+
+

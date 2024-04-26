9dd84b8ac7c923ff25a31b078227f6ee23c4a70d
cosmic3d <chuslitle@gmail.com>
2024-04-26 12:53:59 +0200

    framerate now is being used correctly

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apresas- <apresas-@student.42barcel>       +#+  +:+       +#+        */
+/*   By: jenavarr <jenavarr@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 19:23:55 by apresas-          #+#    #+#             */
-/*   Updated: 2024/04/23 20:02:47 by apresas-         ###   ########.fr       */
+/*   Updated: 2024/04/26 12:48:14 by jenavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	game_loop(t_data *data)
 {
 	render(data);
 	data->frame++;
-	if (data->frame % 5 == 0)
+	if (data->frame % data->sprite.framerate == 0)
 	{
 		data->sprite.i = (data->sprite.i + 1) % data->sprite.frames;
 	}

48498ff14e0ce5a2e1220658a64c4a45f17cbd2f
apresas- <albertprpu97@gmail.com>
2024-04-30 15:44:39 +0200

    Moved game_loop to its own file (2)

---
+++ src/game_loop.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_loop.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apresas- <apresas-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 15:40:42 by apresas-          #+#    #+#             */
+/*   Updated: 2024/04/30 15:44:22 by apresas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	game_loop(t_data *data)
+{
+	render(data);
+	data->frame++;
+	if (data->frame % data->sprite.framerate == 0)
+	{
+		data->sprite.i = (data->sprite.i + 1) % data->sprite.frames;
+	}
+	return (0);
+}
